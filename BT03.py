fe02e2942ea6e93e4574dc10d15ba7cefefaa2d1
NomNom24 <namuy24@gmail.com>
2026-05-28 17:34:17 +0700

    Add files via upload

---
+++ BT03.py
@@ -0,0 +1,20 @@
+
+print("-------------------------------------------")
+print("   HỆ THỐNG TIẾP NHẬN BỆNH NHÂN")
+print("--------------------------------------------")
+
+patient_name = input("Nhập họ tên bệnh nhân: ")
+medical_record_code = input("Nhập mã bệnh án: ")
+department_name = input("Nhập khoa/phòng khám: ")
+
+print("\nTiếp nhận bệnh nhân thành công!")
+
+print("\n--------------------------------------------")
+print("      PHIẾU KHÁM BỆNH ĐIỆN TỬ")
+
+print(f"Họ và tên bệnh nhân : {patient_name}")
+print(f"Mã bệnh án          : {medical_record_code}")
+print(f"Khoa/Phòng khám     : {department_name}")
+
+print("Vui lòng chờ đến lượt khám bệnh.")
+print("--------------------------------------------")
